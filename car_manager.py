9f7ace843a6796e6e6e9936d07899703ad532549
Supriya Tiwari <mesupriya.tiwari@gmail.com>
2026-03-28 13:24:04 +0530

    Initial upload

---
+++ car_manager.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+import random
+colors = ["red","orange","yellow","green","blue","purple"]
+starting_move_distance = 5
+move_increament = 10
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = starting_move_distance
+    def create_cars(self):
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1,stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(colors))  
+            random_y = random.randint(-250,250)  
+            new_car.goto(300,random_y)
+            self.all_cars.append(new_car)
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += move_increament        
---
+++ player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+starting_position = (0,-280)
+move_distnace = 10
+finish_line_y = 280
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def go_to_start(self):
+        self.goto(starting_position)    
+
+    def go_up(self):
+        self.forward(move_distnace) 
+      
+
+    def is_at_finish_line(self):
+        if self.ycor() > finish_line_y: 
+            return True
+        else:
+            return False     
+
